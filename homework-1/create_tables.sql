6e62baf1445ace4aba917f40dcaf5959f6ba5ece
Dmitry Dronov <cskafan1911@gmail.com>
2023-10-01 20:50:36 +0300

    Отредактирован sql-код из pgAdmin в файл create_tables.sql

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -4,7 +4,7 @@ CREATE TABLE employees
 	first_name varchar(50),
 	last_name varchar(50),
 	title varchar(100),
-	birth_date varchar(10),
+	birth_date date,
 	notes text
 );
 
@@ -20,6 +20,6 @@ CREATE TABLE orders
 	order_id int PRIMARY KEY NOT NULL,
 	customer_id varchar(5) REFERENCES customers(customer_id),
 	employee_id int REFERENCES employees(employee_id),
-	order_date varchar(10),
+	order_date date(10),
 	ship_city varchar(50)
 );
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,29 @@
-"""Скрипт для заполнения данными таблиц в БД Postgres."""
+from csv import DictReader
+import psycopg2
+
+
+conn = psycopg2.connect(
+    host='localhost',
+    database='north',
+    user='postgres',
+    password='t95qsnapQ')
+
+file_csv = "C:\\Users\\Dima\\postgres-homeworks\\homework-1\\north_data\\employees_data.csv"
+
+with open(file_csv, 'r', encoding='utf-8-sig') as file:
+    reader = DictReader(file)
+    for line in reader:
+        employees = (line['employee_id'], line['first_name'], line['last_name'], line['title'], line['birth_date'], line['notes'])
+
+        conn = psycopg2.connect(
+            host='localhost',
+            database='north',
+            user='postgres',
+            password='t95qsnapQ')
+        try:
+            with conn:
+                with conn.cursor() as cur:
+                    cur.execute('INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)', employees)
+        finally:
+            conn.close()
+ 
