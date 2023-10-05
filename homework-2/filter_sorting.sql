eed58de5d83d36e28a7b860fac6bee51e4135f5b
Dmitry Dronov <cskafan1911@gmail.com>
2023-10-05 21:03:28 +0300

    Задание queries

--- homework-2/filter_sorting.sql
+++ homework-2/filter_sorting.sql
@@ -9,7 +9,7 @@ ORDER BY ship_country, ship_city
 
 -- 3. сколько дней в среднем уходит на доставку товара в Германию (таблица orders, колонки order_date, shipped_date, ship_country)
 SELECT AVG(shipped_date - order_date) FROM orders
-WHERE ship_country <> 'Germany'
+WHERE ship_country = 'Germany'
 
 -- 4. минимальную и максимальную цену среди продуктов, не снятых с продажи (таблица products, колонки unit_price, discontinued не равно 1)
 SELECT MIN(unit_price), MAX(unit_price) FROM products
